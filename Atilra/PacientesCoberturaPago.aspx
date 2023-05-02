ded11565ffb48439b0a7471a8ace4c1b6cd4824e
lidioguedez <lidioguedez-noreply@lidioguedez.com>
2023-05-02 15:51:55 -0300

    Pacientes con coverturas

---
+++ Atilra/PacientesCoberturaPago.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Pacientes con cobertura que pagaron estudios" Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="PacientesCoberturaPago.aspx.cs" Inherits="Atilra.PacientesCoberturaPago" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Pacientes con cobertura que pagaron estudios</h2>
+    <div class="row">
+        <div class="col-sm-6">
+            <asp:GridView ID="GridViewPacientesCoberturaPago" runat="server" CssClass="table table-striped table-hover" AutoGenerateColumns="true">
+
+            </asp:GridView>
+        </div>
+    </div>
+</asp:Content>
---
+++ Atilra/PacientesCoberturaPago.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Atilra
+{
+    public partial class PacientesCoberturaPago : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarPacientesCoberturaPago();
+            }
+        }
+
+        private void CargarPacientesCoberturaPago()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Top3_Pacientes_Pago_Estudios", connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    GridViewPacientesCoberturaPago.DataSource = table;
+                    GridViewPacientesCoberturaPago.DataBind();
+                }
+            }
+        }
+    }
+}

7016f8af093891b579913f24016012e60131e00d
lidioguedez <lidioguedez-noreply@lidioguedez.com>
2023-05-02 15:50:49 -0300

    add estudios x periodos

---
+++ Atilra/EstudiosPeriodo.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" EnableEventValidation="false" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="EstudiosPeriodo.aspx.cs" Inherits="Atilra.EstudiosPeriodo" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="row">
+            <h1 class="mb-4">Estudios por período</h1>
+            <p>Por favor ingrese el período (mes y año) para consultar la cantidad de pacientes y médicos que realizaron estudios:</p>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group mb-3">
+                        <label for="TextBoxMes">Mes:</label>
+                        <asp:TextBox ID="TextBoxMes" runat="server" CssClass="form-control" ViewStateMode="Disabled"/>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group mb-3">
+                        <label for="TextBoxAnio">Año:</label>
+                        <asp:TextBox ID="TextBoxAnio" runat="server" CssClass="form-control" ViewStateMode="Disabled"/>
+                    </div>
+                </div>
+            </div>
+            <asp:Button ID="ButtonConsultar" runat="server" Text="Consultar" CssClass="btn btn-primary mx-2 mt-4 col-sm-1" OnClick="ButtonConsultar_Click" />
+            <br />
+            <asp:Label ID="LabelResult" runat="server" ForeColor="Green" CssClass="mt-3 d-block" />
+        </div>
+
+    </div>
+</asp:Content>
---
+++ Atilra/EstudiosPeriodo.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Atilra
+{
+    public partial class EstudiosPeriodo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonConsultar_Click(object sender, EventArgs e)
+        {
+            int mes = Convert.ToInt32(TextBoxMes.Text);
+            int anio = Convert.ToInt32(TextBoxAnio.Text);
+            int cantidadPacientes, cantidadMedicos;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("sp_cantidad_pacientes_y_medicos_por_periodo", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@mes", mes);
+                    command.Parameters.AddWithValue("@anio", anio);
+                    SqlParameter cantidadPacientesParam = new SqlParameter("@cantidadPacientes", SqlDbType.Int);
+                    cantidadPacientesParam.Direction = ParameterDirection.Output;
+                    command.Parameters.Add(cantidadPacientesParam);
+
+                    SqlParameter cantidadMedicosParam = new SqlParameter("@cantidadMedicos", SqlDbType.Int);
+                    cantidadMedicosParam.Direction = ParameterDirection.Output;
+                    command.Parameters.Add(cantidadMedicosParam);
+
+                    command.ExecuteNonQuery();
+
+                    cantidadPacientes = (int)cantidadPacientesParam.Value;
+                    cantidadMedicos = (int)cantidadMedicosParam.Value;
+                }
+            }
+
+            LabelResult.Text = $"En el período {mes}/{anio}, {cantidadPacientes} pacientes y {cantidadMedicos} médicos realizaron estudios.";
+        }
+    }
+}
